0a358fc2edde1d7edc3081e055d3844390a639ae
diegovelasquez-g <velasquez.alejandrod@gmail.com>
2021-09-15 16:21:41 -0600

    Parte1

--- App.js
+++ App.js
@@ -1,21 +1,107 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+/**
+* @format
+* @flow strict-local
+*/
+import React, { useState,useEffect} from 'react';
+import {
+ SafeAreaView,
+ StyleSheet,
+ View,
+ Text,
+ StatusBar,
+} from 'react-native';
+import colors from './src/utilis/colors';
+import Form from './src/Components/Form';
+import Footer from './src/Components/Footer';
+import Result from './src/Components/Result';
+export default function App(){
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const[capital,setCapital] = useState(null);
+  const[interest,setInterest] = useState(null);
+  const[months,setMonths] = useState(null);
+  const [total, setTotal] = useState(null);
+ const [errorMessage, setErrorMessage] = useState('');
+useEffect(() => {
+ if (capital && interest && months) calculate();
+ else reset();
+ }, [capital, interest, months]);
+ const calculate = () => {
+ reset();
+ if (!capital) {
+  setErrorMessage('Añade la cantidad que quieres solicitar');
+  } else if (!interest) {
+  setErrorMessage('Añade el interes del prestamos');
+  } else if (!months) {
+  setErrorMessage('Seleccióna los meses a pagar');
+  } else {
+  const i = interest / 100;
+  const fee = capital / ((1 - Math.pow(i + 1, -months)) / i);
+  setTotal({
+  monthlyFee: fee.toFixed(2).replace('.', ','),
+  totalPayable: (fee * months).toFixed(2).replace('.', ','),
+  });
+  }
+  };
+  const reset = () => {
+  setErrorMessage('');
+  setTotal(null);
+  };
+
+return(
+ <>
+ <StatusBar barStyle="light-content" />
+ <SafeAreaView style={styles.Header}>
+ <Text style={styles.HeadApp}>Cotizador de Prestamos</Text>
+ <Form
+  setCapital ={setCapital}
+  setInterest ={setInterest}
+  setMonths = {setMonths}
+ 
+ />
+ </SafeAreaView>
+ 
+ <Result
+ capital={capital}
+ interest={interest}
+ months={months}
+ total={total}
+ errorMessage={errorMessage}
+ />
+<Footer calculate={calculate} />
+ </>
+ );
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+ Header:{
+ backgroundColor: colors.PRIMARY_COLOR,
+ height:200,
+ borderBottomLeftRadius:30,
+ borderBottomRightRadius:30,
+ alignItems:'center'
+ },
+ HeadApp:{
+   fontSize:25,
+   fontWeight: 'bold',
+   color: '#fff',
+   marginTop:15,
+ },
+ titleApp: {
+  fontSize: 25,
+  fontWeight: 'bold',
+  color: '#fff',
+  marginTop: 15,
   },
-});
+  background: {
+    backgroundColor: colors.PRIMARY_COLOR,
+    height: 200,
+    width: '100%',
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius: 30,
+    position: 'absolute',
+    zIndex: -1,
+    },
+    safeArea: {
+      height: 290,
+      alignItems: 'center',
+      },
+})
